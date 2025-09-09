24c0a2c9da3f9953ab2f87bd3121c455dfdc86ef
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2025-09-09 00:25:42 +0000

    Add some packages.

--- configuration.nix
+++ configuration.nix
@@ -23,4 +23,21 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
+  environment.systemPackages = with pkgs; [
+    #Fuzzing
+    aflplusplus
+    clang
+    gcc
+
+    #Utilities
+    silver-searcher
+    git
+    neovim
+    python3
+
+    #Static analysis
+    ##Cyclomatic complexity
+    #python3Packages.lizard
+    flawfinder
+  ];
 }
