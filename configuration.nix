75db724aab4247e25c8939c67f328ffb243c1f39
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2025-11-16 06:40:47 +0000

    Enable flakes.

--- configuration.nix
+++ configuration.nix
@@ -34,6 +34,10 @@
   #Necessary for VSCode
   programs.nix-ld.enable = true;
 
+  nix.extraOptions = ''
+  experimental-features = nix-command flakes
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
